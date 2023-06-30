76b4d93bcde12e38534d108c02abb54cc99bb990
Stephen <68921584+Kafolabi@users.noreply.github.com>
2023-06-30 10:55:41 +0100

    Update PortfolioBody.js
    Reduced the margin of the portfolio cards

--- src/components/PortfolioBody.js
+++ src/components/PortfolioBody.js
@@ -1,6 +1,6 @@
 const PortfolioBody = (props) => {
   return (
-    <div className="card-body mx-5">
+    <div className="card-body mx-2">
       <h2 className="fw-bolder">{props.heading}</h2>
       <p className="fs-5 opacity-75">{props.paragraph}</p>
     </div>
